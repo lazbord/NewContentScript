a9ba64da83e6df1869feb4f757ed1d97d32ec744
lazbord <lazarebordereaux@yahoo.fr>
2024-01-09 16:47:36 +0100

    retrieve username data from any website v2

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
-let userInput = "default"; // Declare userInput outside the listeners
+let userInput = "default";
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.userInput) {
-        userInput = message.userInput; // Update the global userInput
+        userInput = message.userInput;
         console.log("background received : " + userInput);
     }
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    // Respond to 'get-user-data' message with the updated userInput
     if (message === 'get-user-data') {
         sendResponse(userInput);
         console.log("background send :" + userInput);
--- contentScript.js
+++ contentScript.js
@@ -1,21 +1,20 @@
-// Attach an event listener to the input element
-userNameInput.addEventListener("input", handleInputChange);
-
 let user_input = "default";
 
 const inputElements = document.getElementsByTagName("input");
 
 for (let i = 0; i < inputElements.length; i++) {
-    if (inputElements[i].id.toLowerCase().includes("user")) {
-        console.log(`Input element with ID "${inputElements[i].id}" contains 'user'.`);
-        user_input = inputElements[i].id;
+    const elementType = inputElements[i].type.toLowerCase();
+    if (elementType === "text" || elementType === "email") {
+        console.log(`Input element with type "${elementType}" found.`);
+        user_input = inputElements[i];
     }
 }
 
 function handleInputChange() {
-    // Retrieve the value of the input field
     const userInput = user_input.value;
     chrome.runtime.sendMessage({ userInput: userInput });
     console.log("Content script send" + userInput);
 }
+
+userNameInput.addEventListener("input", handleInputChange);
 
